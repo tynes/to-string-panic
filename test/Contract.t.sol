7f6496b040426df66013422218b5645f7c9a6b23
Mark Tyneway <mark.tyneway@gmail.com>
2022-07-11 14:01:52 -0700

    noted panic case

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
-
-    function testExample() public {
-        assertTrue(true);
+    function testExample(bytes32 data) public {
+        // This causes a panic
+        vm.toString(data);
     }
 }
